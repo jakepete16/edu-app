f52ec2fc63e74278c278e0b4afdb3f5a56c9ebc7
jakepete16 <106332877+jakepete16@users.noreply.github.com>
2022-06-08 09:29:44 -0400

    Delete services directory

--- services/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./authService";
-export * from "./firebaseService";
-export * from "./firestoreService";
-export * from "./handshakeService";
-export * from "./studentTeacherService";
-export * from "./storageService";
-export * from "./hiredPlacementsService";
